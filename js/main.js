19cea920ccf45e5ba6b090edc8d2b839d72503e4
quynam <quynamle123@gmail.com>
2020-03-05 12:57:31 +0700

    da tao ra cai store

--- js/main.js
+++ js/main.js
@@ -1,4 +1,62 @@
 import store from './store.js';
 
-console.log("main js loaded");
-console.log(store.name);
+const renderPage = () =>{
+    const rootEl = document.getElementById("root");
+    const state = store.getState();
+    const { activePage } = state;
+    const navBar = `
+        <ul>
+            <li class="nav-home">Home</li>
+            <li class="nav-about">About</li>
+            <li class="nav-products">Products</li>
+        </ul>
+    `;
+
+    const home=`
+        ${navBar}
+        <div>
+            <h1>This is home Page</h1>
+        </div>
+    `;
+
+    const about = `
+        ${navBar}
+        <div>
+            <h1>This is About page</h1>
+        </div>
+    `;
+
+    const products = `
+        ${navBar}
+        <div>
+            <h1>This is products page</h1>
+        </div>
+    `;
+    //bind event for navBar click
+    document.onclick = event =>{
+        const target = event.target;
+        if(target.className==="nav-home") navigate("home");
+        else if (target.className === "nav-about") navigate("about");
+        else if (target.className === "nav-products") navigate("products");
+    };
+
+    let template ="";
+    if (activePage === "home") template = home;
+    else if (activePage === "about") template = about;
+    else if (activePage === "products") template = products;
+
+    rootEl.innerHTML = template;
+};
+
+const navigate = page =>{
+    const state = store.getState();
+    const newState = {
+        ...state,
+        activePage:page
+    };
+    store.setState(newState);
+};
+renderPage();
+store.subcribe("stateChanged",renderPage);
+
+
--- js/store.js
+++ js/store.js
@@ -1,4 +1,33 @@
-console.log("store.js loaded");
+let state = {
+    activePage: 'home',
+    productDetail:null,
+    shoppingCart:[],
+    products:[]
+}
+
+const events = {}
+
+const subcribe = (eventName, callback) =>{
+    if(!events.hasOwnProperty(eventName)) events[eventName] = [];
+    events[eventName].push(callback)
+}
+
+const publish = (eventName, data)=>{
+    if(!events.hasOwnProperty(eventName)) return;
+    events[eventName].map(callback =>{
+        callback(data);
+    });
+}
+
+const getState = () => state 
+
+const setState = newState =>{
+    state = newState;
+    publish("stateChanged", newState)
+}
+
 export default{
-    name:"store"
-};
+    getState,
+    setState,
+    subcribe
+}
